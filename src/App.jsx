b5a6f3c12e301cf5558f36f047253bacd223cfd3
ad-dw <aditya_dwivedi01@outlook.com>
2025-05-22 12:16:30 +0530

    Migrating to functional components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import SearchBox from "./components/searchComponent/search.component";
 import UserList from "./components/userListComponent/userList.component";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      searchValue: "",
-    };
-  }
+const App = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState(users);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((data) => this.setState({ users: data }));
-  }
+      .then((data) => setUsers(data));
+  }, []);
 
-  handleSearchChange(event) {
-    this.setState({ searchValue: event.target.value.toLowerCase() });
-  }
+  useEffect(() => {
+    setFilteredUsers(
+      users.filter((user) => user.name.toLowerCase().includes(searchValue))
+    );
+  }, [users, searchValue]);
 
-  render() {
-    const filteredUsers = this.state.users.filter((user) =>
-      user.name.toLowerCase().includes(this.state.searchValue)
-    );
-    return (
-      <div className="App">
-        <SearchBox handleSearchChange={this.handleSearchChange.bind(this)} />
-        {!!filteredUsers.length && <UserList users={filteredUsers} />}
-      </div>
-    );
-  }
-}
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value.toLowerCase());
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Users Rolodex</h1>
+      <SearchBox handleSearchChange={handleSearchChange} />
+      {!!filteredUsers.length && <UserList users={filteredUsers} />}
+    </div>
+  );
+};
 
 export default App;
--- src/components/SearchComponent/search.component.tsx
+++ src/components/SearchComponent/search.component.tsx
@@ -1,17 +1,12 @@
-import { Component } from "react";
 import "./search.styles.css";
 
-class SearchBox extends Component {
-  render() {
-    return (
-      <input
-        className="search-box"
-        type="search"
-        placeholder="Search user"
-        onChange={this.props.handleSearchChange}
-      />
-    );
-  }
-}
+const SearchBox = ({ handleSearchChange }) => (
+  <input
+    className="search-box"
+    type="search"
+    placeholder="Search user"
+    onChange={handleSearchChange}
+  />
+);
 
 export default SearchBox;
